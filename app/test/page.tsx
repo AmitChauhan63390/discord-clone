98f61fda9f0172b9e926d33e4d312eeb4641ec56
amit <amit63390@gmail.com>
2024-09-06 13:25:37 +0530

    initial modal added

---
+++ app/test/page.tsx
@@ -0,0 +1,9 @@
+
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
---
+++ components/modals/initial-modal.tsx
@@ -0,0 +1,104 @@
+"use client"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { Button } from "../ui/button"
+import { useEffect, useState } from "react"
+
+
+
+const formSchema = z.object({
+    name:z.string().min(1,{
+        message:"Server name is required"
+    }),
+    imageUrl:z.string().min(1,{
+        message:"Server image is required"
+    })
+})
+const InitialModal = () => {
+
+    const [isMounted,setIsMounted]=useState(false)
+    
+    useEffect(()=>{
+        setIsMounted(true)
+    },[])
+
+    const form =useForm({
+        resolver:zodResolver(formSchema),
+        defaultValues:{
+            name:"",
+            imageUrl:"",
+        }
+    })
+
+    const isLoading = form.formState.isSubmitting
+
+    const onSubmit=async(values:z.infer<typeof formSchema>)=>{
+        console.log(values)
+    }
+
+    if(!isMounted){
+        return null;
+    }
+  return (
+    <div>
+        <Dialog open>
+            <DialogContent className="bg-white text-black p-0 overflow-hidden">
+
+                <DialogHeader className="pt-8 px-6">
+                    <DialogTitle className="font-bold text-2xl text-center text-zinc">Create Your Server</DialogTitle>
+                    <DialogDescription className="text-center text-zinc-500">
+                        Give your server a personality with a name and an image. you can always change it later.
+                    </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="p-6 space-y-8">
+                        <div className="space-y-8 px-6">
+                            <div className="flex items-center justify-center text-center">
+                                TODO:IMAGE UPLOAD
+
+                            </div>
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem className="flex flex-col">
+                                        <FormLabel className="text-zinc-500 font-bold uppercase text-xs dark:text-secondary/70">Server Name</FormLabel>
+                                        <FormControl>
+                                            <input
+                                            disabled={isLoading}
+                                                type="text"
+                                                placeholder="Enter server name"
+                                                className="bg-zinc-300/50 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0 p-2 "
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                        
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <DialogFooter className="bg-gray-100 px-6 py-4">
+                            <Button variant="primary" disabled={isLoading}>
+                                Create
+
+                            </Button>
+                        </DialogFooter>
+                    </form>
+
+
+                </Form>
+
+
+            </DialogContent>
+        </Dialog>
+
+    </div>
+  )
+}
+
+export default InitialModal
