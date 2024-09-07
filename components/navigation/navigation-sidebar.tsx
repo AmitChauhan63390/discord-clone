527a9e9c23b198d242af3ac3fd0550919bacf0d9
amit <amit63390@gmail.com>
2024-09-07 23:30:18 +0530

    Modetoggle userbutton

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,11 @@ import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import NavigationItem from "./navigation-item";
+import { ModeToggle } from "../mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSidebar=async ()=>{
     const  profile=await currentProfile();
@@ -24,6 +29,25 @@ export const NavigationSidebar=async ()=>{
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-3">
             <NavigationAction/>
+            <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto "/>
+            <ScrollArea className="flex-1 w-full ">
+               {servers.map((server)=>{
+                   return (<div key={server.id} className="mb-4">
+
+                       <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl}/>
+
+                   </div>)
+               })}
+
+            </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle/>
+                <UserButton appearance={{
+                    elements:{
+                        avatarBox:"h-[48px] w-[48px]"
+                    }
+                }}/>
+            </div>
         </div>
     )
 }
