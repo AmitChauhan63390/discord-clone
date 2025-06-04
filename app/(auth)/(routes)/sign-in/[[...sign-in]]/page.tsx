fbc7aff69382d73f7450d127b7a15f0d78188adc
AmitChauhan63390 <amit63390@gmail.com>
2025-06-04 13:27:43 +0530

    centered div

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return <div className="flex items-center justify-center min-h-screen">
+  <SignIn />
+</div>
+
 }
--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,8 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return <div className="flex items-center justify-center min-h-screen">
+  <SignUp />
+</div>
+
 }
